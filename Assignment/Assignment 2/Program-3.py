352d5e3c60d7721866c635fc2e2d90098c30c762
SpreadSheets600 <SpreadSheets600@users.noreply.github.com>
2025-08-12 14:10:18 +0000

    deploy: b12e5c37fbbb1037295bb76281de4353ad8f0bfb

---
+++ Assignment/Assignment 2/Program-3.py
@@ -0,0 +1,6 @@
+# Program 3 : Write a program to convert list of strings to integers.
+
+lst = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
+intList = [int(x) for x in lst]
+print("Converted List of Integers : ", intList)
