ea22e783377c8595a887b875bfdb32e5f982e95c
Soham Maity <sohammaity006@gmail.com>
2025-09-02 14:29:40 +0000

    Add Program-3 to demonstrate adding and updating key-value pairs in a dictionary

---
+++ Assignment/Assignment 4/Program-3.py
@@ -0,0 +1,8 @@
+# Write a program to add a new key–value pair in a dictionary and update the value of an existing key.
+
+fruits = {"apple": 100, "banana": 40, "mango": 120}
+
+print("Original Dictionary :", fruits)
+
+fruits["orange"] = 80
+print("\nAfter Updating Dictionary ~\n", fruits)
