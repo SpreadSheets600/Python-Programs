3f46384b6243dc7f84933cc4fd19fb8847711864
Soham Maity <sohammaity006@gmail.com>
2025-11-03 19:00:43 +0530

    Add Program-6 to check whether a given number is an Armstrong number

---
+++ Assignment/Assignment 5/Program-6.py
@@ -0,0 +1,16 @@
+# Write a Python program to check whether a given number is an Armstrong number or not.
+
+
+def is_armstrong_number(num):
+    num_str = str(num)
+    num_digits = len(num_str)
+    sum_of_powers = sum(int(digit) ** num_digits for digit in num_str)
+    return sum_of_powers == num
+
+
+number = int(input("Enter a number: "))
+
+if is_armstrong_number(number):
+    print(f"{number} is an Armstrong number.")
+else:
+    print(f"{number} is not an Armstrong number.")
